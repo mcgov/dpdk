90c803cc5e1da84e5b47ce59279ded2701ec6514
Matthew G. McGovern <matthew@mcgov.dev>
2024-05-03 17:01:05 -0700

    add mac fetch

--- examples/devname/main.c
+++ examples/devname/main.c
@@ -20,63 +20,82 @@ const char usage_info[] = "usage: dpdk-devname\n";
 /* Initialization of Environment Abstraction Layer (EAL). 8< */
 int main(int argc, char **argv)
 {
-    int ret;
+	int ret;
 
-    ret = rte_eal_init(argc, argv);
-    if (ret < 0)
-        rte_panic("Cannot init EAL\n");
-    /* >8 End of initialization of Environment Abstraction Layer */
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		rte_panic("Cannot init EAL\n");
+	/* >8 End of initialization of Environment Abstraction Layer */
 
-    struct rte_eth_dev_info device_info;
-    char device_name_by_port[RTE_ETH_NAME_MAX_LEN] = {0};
-    for (uint16_t portid = 0; portid < RTE_MAX_ETHPORTS; portid++)
-    {
-        if (!rte_eth_dev_is_valid_port(portid))
-            continue;
+	struct rte_eth_dev_info device_info;
+	char device_name_by_port[RTE_ETH_NAME_MAX_LEN] = {0};
+	for (uint16_t portid = 0; portid < RTE_MAX_ETHPORTS; portid++)
+	{
+		if (!rte_eth_dev_is_valid_port(portid))
+			continue;
 
-        ret = rte_eth_dev_info_get(portid, &device_info);
-        if (ret < 0)
-        {
-            fprintf(stderr,
-                    "Invalid or no info for port %i, err: %s\n",
-                    portid, rte_strerror(ret));
-            continue;
-        }
-        ret = rte_eth_dev_get_name_by_port(portid, device_name_by_port);
-        if (ret < 0)
-        {
-            fprintf(stderr,
-                    "No name info returned for port %i, err: %s\n",
-                    portid, rte_strerror(ret));
-            continue;
-        }
-        struct rte_eth_dev_owner device_owner;
-        ret = rte_eth_dev_owner_get(portid, &device_owner);
-        if (ret < 0)
-        {
-            fprintf(stderr, "Could not get ownership for port %i (%s)\n",
-                    portid, device_name_by_port);
-            memset(&device_owner, 0, sizeof(struct rte_eth_dev_owner));
-        }
+		ret = rte_eth_dev_info_get(portid, &device_info);
+		if (ret < 0)
+		{
+			fprintf(stderr,
+					"Invalid or no info for port %i, err: %s\n",
+					portid, rte_strerror(ret));
+			continue;
+		}
+		ret = rte_eth_dev_get_name_by_port(portid, device_name_by_port);
+		if (ret < 0)
+		{
+			fprintf(stderr,
+					"No name info returned for port %i, err: %s\n",
+					portid, rte_strerror(ret));
+			continue;
+		}
+		struct rte_eth_dev_owner device_owner;
+		ret = rte_eth_dev_owner_get(portid, &device_owner);
+		if (ret < 0)
+		{
+			fprintf(stderr, "Could not get ownership for port %i (%s)\n",
+					portid, device_name_by_port);
+			memset(&device_owner, 0, sizeof(struct rte_eth_dev_owner));
+		}
+		
+		if (!device_owner.name[0])
+			strcpy(device_owner.name, "null");
+		
+		struct rte_ether_addr macaddr;
+		ret = rte_eth_macaddr_get(portid, &macaddr);
+		if (ret < 0)
+		{
+			fprintf(stderr, "Could not get macaddr info for port %i (%s)\n",
+					portid, device_name_by_port);
+			memset(&macaddr, 0, sizeof(struct rte_ether_addr));
+		}
+		printf("dpdk-devname found port:%i "
+			   "driver:%s "
+			   "eth_dev_info_name:%s "
+			   "get_name_by_port_name:%s "
+			   "owner_id:0x%016lx "
+			   "owner_name:%s "
+			   "mac:%02x:%02x:%02x:%02x:%02x:%02x\n",
+			   portid,
+			   device_info.driver_name,
+			   device_info.device->name,
+			   device_name_by_port,
+			   device_owner.id,
+			   device_owner.name,
+			   macaddr.addr_bytes[0],
+			   macaddr.addr_bytes[1],
+			   macaddr.addr_bytes[2],
+			   macaddr.addr_bytes[3],
+			   macaddr.addr_bytes[4],
+			   macaddr.addr_bytes[5]
+		);
+	}
 
-        fprintf(stdout, "dpdk-devname found port:%i "
-               "driver:%s "
-               "eth_dev_info_name:%s "
-               "get_name_by_port_name:%s "
-               "owner_id:0x%016lx "
-               "owner_name:%s\n",
-               portid,
-               device_info.driver_name,
-               device_info.device->name,
-               device_name_by_port,
-               device_owner.id,
-               device_owner.name);
-    }
+	/* clean up the EAL */
+	rte_eal_cleanup();
 
-    /* clean up the EAL */
-    rte_eal_cleanup();
-
-    return 0;
+	return 0;
 }
 
 // netvsc with matching vmbus device, use that
