66946703fcb14118ce9c967e13f03427bd3d950a
Matthew G McGovern <mamcgove@microsoft.com>
2025-03-07 10:42:51 -0800

    write once

--- app/test-pmd/txonly.c
+++ app/test-pmd/txonly.c
@@ -236,6 +236,7 @@ pkt_burst_prepare(struct rte_mbuf *pkt, struct rte_mempool *mbp,
 		 */
 		src_port = ((0xC0 | fs->tx_queue)<< 8) + (src_var++ & 0xFF);
 		udp_hdr->src_port = rte_cpu_to_be_16(src_port);
+		//RTE_PER_LCORE(_src_port_var) = src_var;
 	}
 
 	if (unlikely(tx_pkt_split == TX_PKT_SPLIT_RND) || txonly_multi_flow)
@@ -377,10 +378,10 @@ pkt_burst_transmit(struct fwd_stream *fs)
 
 	nb_tx = common_fwd_stream_transmit(fs, pkts_burst, nb_pkt);
 
-	if (txonly_multi_flow){
-		RTE_PER_LCORE(_src_port_var) += nb_pkt + nb_tx;
-		printf("src_port_var: %X nb_pkt: %x nb_tx: %x\n", RTE_PER_LCORE(_src_port_var), nb_pkt, nb_tx);
-	}
+	// if (txonly_multi_flow) {
+	// 	// RTE_PER_LCORE(_src_port_var) += nb_pkt ;
+	// 	printf("src_port_var: %x nb_pkt: %x nb_tx: %x\n", RTE_PER_LCORE(_src_port_var), nb_pkt, nb_tx);
+	// }
 
 	if (unlikely(nb_tx < nb_pkt)) {
 		if (verbose_level > 0 && fs->fwd_dropped == 0)
