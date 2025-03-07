718d31ebcdb2f017f40644764b3d93dfac81b5ca
Matthew G McGovern <mamcgove@microsoft.com>
2025-03-07 10:07:12 -0800

    debug

--- app/test-pmd/txonly.c
+++ app/test-pmd/txonly.c
@@ -234,7 +234,7 @@ pkt_burst_prepare(struct rte_mbuf *pkt, struct rte_mempool *mbp,
 		 * the lcore ID. As such, the most significant byte will cycle
 		 * through 0xC0 and 0xFF.
 		 */
-		src_port = ((0xC0 | fs->tx_queue)<< 8) | src_var++;
+		src_port = ((0xC0 | fs->tx_queue)<< 8) + (src_var++ & 0xFF);
 		udp_hdr->src_port = rte_cpu_to_be_16(src_port);
 	}
 
@@ -377,8 +377,10 @@ pkt_burst_transmit(struct fwd_stream *fs)
 
 	nb_tx = common_fwd_stream_transmit(fs, pkts_burst, nb_pkt);
 
-	if (txonly_multi_flow)
-		RTE_PER_LCORE(_src_port_var) -= nb_pkt + nb_tx;
+	if (txonly_multi_flow){
+		RTE_PER_LCORE(_src_port_var) += nb_pkt + nb_tx;
+		printf("src_port_var: %X nb_pkt: %x nb_tx: %x\n", RTE_PER_LCORE(_src_port_var), nb_pkt, nb_tx);
+	}
 
 	if (unlikely(nb_tx < nb_pkt)) {
 		if (verbose_level > 0 && fs->fwd_dropped == 0)
